8a73c3b21cc99a045fdfcfb495a5d837fc59ca38
manasm11 <53897110+manasm11@users.noreply.github.com>
2025-03-28 21:23:35 +0530

    Update setup-manaslab.sh

--- setup-manaslab.sh
+++ setup-manaslab.sh
@@ -62,8 +62,10 @@ install_nvim() {
 install_go() {
         sudo apt purge golang
         curl -LO https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz && \
-        rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz && \
-        export PATH="$PATH:/usr/local/go/bin" && \
+        rm -rf /usr/local/go && sudo tar -C $HOME -xzf go${GO_VERSION}.linux-amd64.tar.gz && \
+        mv $HOME/go $HOME/.go && \
+        export PATH="$HOME/.go/bin:$PATH" && \
+        export GOPATH="$HOME/.go" && \
         rm go${GO_VERSION}.linux-amd64.tar.gz && \
         go install golang.org/x/tools/gopls@latest
 }
