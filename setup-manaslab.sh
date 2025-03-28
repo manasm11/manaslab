865a445df131ea0f1df5dd4faaacd4263de42505
manasm11 <53897110+manasm11@users.noreply.github.com>
2025-03-28 07:38:16 +0530

    Rearranged functions

--- setup-manaslab.sh
+++ setup-manaslab.sh
@@ -16,15 +16,26 @@ main() {
 
 is_debian_based=$([ -f /etc/debian_version ])
 
+download_bashrc() {
+        wget -O "$HOME/.bashrc" \
+                        "https://raw.githubusercontent.com/manasm11/manaslab/refs/heads/main/.bashrc"
+}
+
 download_spellcheck_dictionary() {
         mkdir -p "$HOME/.config/nvim/spell/" && \
         wget -O "$HOME/.config/nvim/spell/en.utf-8.add" \
                         "https://raw.githubusercontent.com/manasm11/manaslab/refs/heads/main/en.utf-8.add"
 }
 
-download_bashrc() {
-        wget -O "$HOME/.bashrc" \
-                        "https://raw.githubusercontent.com/manasm11/manaslab/refs/heads/main/.bashrc"
+download_nvim_conf_deb() {
+        mkdir -p "$HOME/.config/nvim/" && \
+        wget -O "$HOME/.config/nvim/init.vim" \
+                        "https://raw.githubusercontent.com/manasm11/neovim-config/refs/heads/main/init.vim"
+}
+
+install_programs_deb() {
+        sudo apt-get update && sudo apt-get upgrade -y && \
+        sudo apt-get -y install wget curl git
 }
 
 install_nvim() {
@@ -35,29 +46,18 @@ install_nvim() {
         rm nvim-linux-x86_64.tar.gz
 }
 
-install_programs_deb() {
-        sudo apt-get update && sudo apt-get upgrade -y && \
-        sudo apt-get -y install wget curl git
-}
-
-download_nvim_conf_deb() {
-        mkdir -p "$HOME/.config/nvim/" && \
-        wget -O "$HOME/.config/nvim/init.vim" \
-                        "https://raw.githubusercontent.com/manasm11/neovim-config/refs/heads/main/init.vim"
-}
-
 install_vim_plug() {
         sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 }
 
+setup_spellcheck() {
+        nvim -u NONE -es -c "mkspell! $HOME/.config/nvim/spell/en.utf-8.add"
+}
+
 setup_git() {
         git config --global user.email "manas.m22@gmail.com" && \
         git config --global user.name "Manas Mishra"
-}
-
-setup_spellcheck() {
-        nvim -u NONE -es -c "mkspell! $HOME/.config/nvim/spell/en.utf-8.add"
 }
 
 main
