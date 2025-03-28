d358b48d5f94a9008c17617f8d6e0b2709990085
manasm11 <53897110+manasm11@users.noreply.github.com>
2025-03-28 08:26:14 +0530

    Update setup-manaslab.sh

--- setup-manaslab.sh
+++ setup-manaslab.sh
@@ -46,6 +46,7 @@ install_programs_deb() {
 }
 
 install_nvim() {
+        sudo rm /usr/bin/nvim
         curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz && \
         sudo rm -rf /opt/nvim && \
         sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz && \
